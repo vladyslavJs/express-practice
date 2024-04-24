ea5affb6e96b1c85434195bda1903d6974a80769
vladyslavJs <vladyslavmelnyk15@gmail.com>
2024-04-24 13:06:01 +0200

    create-expressApp

--- server.js
+++
@@ -1,9 +0,0 @@
-// const axios = require('axios');
-
-// axios.get('http://localhost:3000/')
-//     .then(response => {
-//         console.log(response.data);
-//     })
-//     .catch(error => {
-//         console.error(error);
-//     });
---
+++ simple-express/routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/login', (req, res, next) => {
+const { email, password } = req.body;
+res.render('response', { title: 'Simple express app', email, password });
+});
+
+module.exports = router;
